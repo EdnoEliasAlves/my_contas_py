83d0b0cb244a299f5751cb0e2961ef945773630f
EdnoEliasAlves <eelias007@gmail.com>
2023-06-01 11:39:30 -0300

    Iniando a cosntrução do Meu App recebendo dois valores e somando

---
+++ meu_app.py
@@ -0,0 +1,13 @@
+# início do sistema
+print("\n\n\t\t++++++++++++++++++++++++++++++++++++++++")
+print("\t\t          ** Ed$Finaças$ **                 ")
+print()
+soma = 0.0  # variável para calcular os valores
+# ...
+valor01 = float(input("Digite o Valor da Primeira Conta "))
+valor02 = float(input("Digite o Valor da Segunda  Conta "))
+# ...
+soma = valor01 + valor02  # operação de cálculo da soma
+# ...
+print("Soma das Contas: ", soma)  # saída no console
+# fim do sistema
