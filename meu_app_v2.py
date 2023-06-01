20be7da7559ff81958322da5ddd8e6dce555bc69
EdnoEliasAlves <eelias007@gmail.com>
2023-06-01 19:36:45 -0300

    Criação da VERSÃO 2 do sistema com a funcionalidade do usuário inserir a quantidade de contas para cálculo

---
+++ meu_app_v2.py
@@ -0,0 +1,20 @@
+# início do sistema
+print("\n\n\t\t++++++++++++++++++++++++++++++++++++++++")
+print("\t\t          ** Ed$Finaças$ **                 ")
+print()
+# declaração de variáveis
+soma = 0.0
+contar_contas = 0
+contas = int(input("Informe a Quantidade de Contas.: "))
+# ...
+# fazendo uso do laço de repetição for
+for conta in range(contas):
+    valor = float(input(f"Digite o Valor da Conta: {conta + 1}.: "))
+    contar_contas = contar_contas + 1
+    soma = soma + valor
+# ...
+# saída no console
+print("\n++++++++++++++++++++++++++++++++++++++++")
+print("Quantidade de Contas  ", contar_contas)
+print("Soma das Contas       ", soma)
+# fim do sistema
