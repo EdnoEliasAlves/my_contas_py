c538fe69b4b82ce3c0e36d25b89fdf771ca23d61
EdnoEliasAlves <eelias007@gmail.com>
2023-06-01 20:19:39 -0300

    Criando a VERSÃO 3 do projeto com inserção de contas sem quantidade prévia, com uma variável de controle, e uma estrutura condicional

---
+++ meu_app_v3.py
@@ -0,0 +1,34 @@
+# início do sistema
+# ...
+# importando bibliotecas
+import sys
+
+
+print("\n\n\t\t++++++++++++++++++++++++++++++++++++++++")
+print("\t\t          ** Ed$Finaças$ **                 ")
+print()
+# declaração de variáveis
+soma = 0.0
+contar_contas = 0
+opcao = -1
+# ...
+# fazendo uso do laço de repetição while
+while opcao != 0:
+    # print("\x1b[2J\x1b[1;1H")
+    valor = float(input("Digite o Valor da Conta ou (0 ZERO) para sair.: "))
+    # print("\x1b[2J\x1b[1;1H")
+    # usando a estrutura de controle
+    if valor == 0:
+        break
+    contar_contas += 1
+    soma += valor
+# ...
+# saída no console
+print("\x1b[2J\x1b[1;1H") # limpar console
+print("\n+++++++++++++++++++++++++++++++++++++++++++++ ")
+print("             ++ S U M Á R I O ++                ")
+print("+++++++++++++++++++++++++++++++++++++++++++++   ")
+print(f"   QUANTIDADE DE CONTAS..... [ 0{contar_contas} ] ")
+print(f"   TOTAL DAS CONTAS......... R$: {soma:.2f}    ")
+sys.exit("\n                  Ed$Finaças$ - Obrigado!\n\n")
+# fim do sistema
